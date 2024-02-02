3a5bfbf15b8bc717ab8c7fef9fdefda3b87bf732
Valerio Pancella <valerio.pancella@gmail.com>
2024-02-02 16:44:22 +0100

    modificato start.sh e modificato url api

--- Frontend1/start.sh
+++ Frontend1/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-docker stop frontend1
-docker rm frontend1
+docker stop frontend1 >/dev/null 2>&1
+docker rm frontend1 >/dev/null 2>&1
+docker image rm frontend1:v1 >/dev/null 2>&1
 docker build -t frontend1:v1 .
 #docker images
 docker run --name frontend1 -d -p 80:80 frontend1:v1
